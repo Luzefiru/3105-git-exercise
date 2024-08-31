c739dcf8aeace6e2f7b23781f4e3daeec7ade575
Luzefiru <ian.deejay.iv@gmail.com>
2024-08-31 13:31:45 +0800

    feat: add greeting prompt

--- index.ts
+++ index.ts
@@ -1,2 +1,8 @@
+import getPrompt from 'prompt-sync';
+const prompt = getPrompt();
+
 console.log('Hello World');
 console.log('I added some changes');
+
+const name = prompt('Identify yourself: ');
+console.log(`Greetings, ${name}!`);
