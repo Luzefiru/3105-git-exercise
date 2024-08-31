38d5df80bf3daae0f5a6c15a50468dbe46c519d9
Luzefiru <ian.deejay.iv@gmail.com>
2024-08-31 13:36:06 +0800

    nit: add honorific suffix

--- index.ts
+++ index.ts
@@ -5,4 +5,4 @@ console.log('Hello World');
 console.log('I added some changes');
 
 const name = prompt('Identify yourself: ');
-console.log(`Greetings, ${name}!`);
+console.log(`Greetings, ${name}-kun!`);
