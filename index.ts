8801805ee73587dff0ee55ee7bd24bc80b97f516
Samuel-Bonghanoy <20103261@usc.edu.ph>
2024-08-31 17:03:28 +0800

    feat: add lifechanging console log

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
-import getPrompt from 'prompt-sync';
+import getPrompt from "prompt-sync";
 const prompt = getPrompt();
 
-console.log('こんにちは世界');
-console.log('いくつか変更を加えました');
+console.log("こんにちは世界");
+console.log("いくつか変更を加えました");
 
-const name = prompt('身元を明かしてください: ');
+console.log("i dont understand what u saying");
+
+const name = prompt("身元を明かしてください: ");
 console.log(`こんにちは, ${name}-くん!`);
